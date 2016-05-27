e4b905e60593be107d80d6c7ac2a15c69c582cf5
Jan P. Guggenbuehler <jan.guggenbuehler@gmail.com>
2016-05-27 17:21:56 +0200

    Update introToCopulas.R

--- introToCopulas.R
+++ introToCopulas.R
@@ -13,30 +13,16 @@ sigmaMatrix <- matrix(c(1.0, 0.7,
                       nrow = d)
 pq <- mvrnorm(n, mu = rep(0, d), Sigma = sigmaMatrix)
 
-# Plotting
+# Transformations
 colnames(pq) <- c('P', 'Q') # name our two r.v.s P and Q
-pdf(file = "C:/Users/Jan/Dropbox/2. University/MA - Semester 2/2016 - Dreamteam/Time Series Analysis/Project Copulas/ProjectPaper/Graphics/eduCopula1.pdf")
 pairs.panels(pq, rug = FALSE, cex.cor = 0.7, hist.col = "dodgerblue4") # plot
-dev.off()
-
 ###########################################################################################################################################################
-
-uv <- pnorm(pq)
-
-# Plotting
+uv <- pnorm(pq) # probability integral transform
 colnames(uv) <- c('U', 'V') # name our two r.v.s U and V
-pdf(file = "C:/Users/Jan/Dropbox/2. University/MA - Semester 2/2016 - Dreamteam/Time Series Analysis/Project Copulas/ProjectPaper/Graphics/eduCopula2.pdf")
 pairs.panels(uv, rug = FALSE, cex.cor = 0.7, hist.col = "dodgerblue4") # plot
-dev.off()
-
 ###########################################################################################################################################################
-
-x <- qt(uv[, 1], df = 7) # transform first rv with quantile function of student's t with nu = 7
-y <- qt(uv[, 2], df = 15) # transform second rv with quantile function of student's t with nu = 15
+x <- qt(uv[, 1], df = 7) # transform (smirnov) first rv with quantile function of student's t with nu = 7
+y <- qt(uv[, 2], df = 15) # transform (smirnov) second rv with quantile function of student's t with nu = 15
 xy <- cbind(x, y) # combine the two matrices by columns
-
-# Plotting
 colnames(xy) <- c('X', 'Y') # name our two r.v.s X and Y
-pdf(file = "C:/Users/Jan/Dropbox/2. University/MA - Semester 2/2016 - Dreamteam/Time Series Analysis/Project Copulas/ProjectPaper/Graphics/eduCopula3.pdf")
 pairs.panels(xy, rug = FALSE, cex.cor = 0.7, hist.col = "dodgerblue4") # plot
-dev.off()
